9decc30eb971af32984ea8330d3943325ac35c1f
EDeepika1306 <152486040+EDeepika1306@users.noreply.github.com>
2024-07-29 19:45:39 +0530

    Else_Staement

---
+++ Control_Statement/03_IF_ELSE.py
@@ -0,0 +1,40 @@
+income=int(input("Enter income:"))
+if(income>7000):
+    print("Scholarship is available")
+else:
+    print("Not eligible for scholarship")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
