910bfd33eddcecd52a6c9e7de0aa18e3b2c5c8e0
EDeepika1306 <152486040+EDeepika1306@users.noreply.github.com>
2024-07-29 19:42:36 +0530

    Else_Statement

---
+++ Control_Statement/02_ELIF.py
@@ -0,0 +1,52 @@
+
+a=int(input("Enter  number:"))
+b=int(input("Enter  number:"))
+print("1.add")
+print("2.sub")
+print("3.mul")
+print("4.div")
+c=int(input("Enter the number for arithmetic opration:"))
+if(c==1):
+    print("Addition is:",a+b)
+elif (c==2):
+    print("Subraction  is:",a-b)
+elif(c==3):
+    print("Multiplecation is",a*b)
+elif(c==4):
+    print("division is:",a/b)
+else:
+    print("ENTER THE VALID NUMBER!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
