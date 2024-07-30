d329067d7de36be040eea39740f152d79fd96eca
EDeepika1306 <152486040+EDeepika1306@users.noreply.github.com>
2024-07-30 21:27:28 +0530

    Program4

---
+++ FOR LOOP/program4.py
@@ -0,0 +1,10 @@
+th_count=0
+fi_count=0
+for i in range(1,111):
+    if(i%3==0):
+        th_count=th_count+1
+    elif (i%5==0):
+        fi_count=fi_count+1
+print(th_count)
+print(fi_count)
+        
