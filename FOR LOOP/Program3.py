7fb9d7b7f5ec5477693478b6fa5c19fe0d0a6194
EDeepika1306 <152486040+EDeepika1306@users.noreply.github.com>
2024-07-30 21:13:59 +0530

    Program3

---
+++ FOR LOOP/Program3.py
@@ -0,0 +1,13 @@
+"""
+Count the number of odd and even numbers between 1 to 10
+"""
+e_count=0
+o_count=0
+for i in range(1,11):
+    if(i%2==0):
+        e_count=e_count+1
+        
+    else:
+        o_count=o_count+1
+print(e_count)
+print(e_count)
